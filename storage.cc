5cd4586a85f13aada47b6226bfdb5ec3f0145b4e
Vinith Venkatesan <vinith@Viniths-MacBook-Pro.local>
2020-02-14 22:11:14 -0600

    checksum code

--- storage.cc
+++ storage.cc
@@ -6,6 +6,17 @@ std::mutex fileMutex;
 
 const std::string FILE_NAME = "data.log";
 
+// djb2 hashing
+uint32_t checksum(std::string str) {
+  uint32_t hashVal = 5381;
+  
+  for(std::string::size_type i = 0; i < str.size(); ++i) {
+    hashVal = ((hashVal << 5) + hashVal) + str[i];
+}
+
+  return hashVal;
+}
+
 void buildTrieFromFile(ConcurrentTrie<std::string> &map) {
 	std::ifstream file(FILE_NAME);
 	if (!file.is_open()) { 
@@ -19,10 +30,21 @@ void buildTrieFromFile(ConcurrentTrie<std::string> &map) {
 			continue;
 		}
 		std::string key = str.substr(0, ind);
-		std::string value = str.substr(ind + 1);
 
-		// TODO: Checksum validation
+		std::size_t ind2 = str.find(' ', ind+1);
+		if(ind2 == std::string::npos) {
+			continue;
+		}
+		std::string value = str.substr(ind + 1, ind2 - ind - 1);
 
+		std::string chksum = str.substr(ind2 + 1);
+
+		uint32_t hashVal = checksum(key+value);
+
+		if(chksum.compare(std::to_string(hashVal))!=0) {
+			continue;
+		}
+		
 		map.insert(key, value, false);
 	}
 	
@@ -54,9 +76,11 @@ void persistData(std::string key, std::string value) {
 			fd = open(cfile, O_WRONLY | O_APPEND);
 		}
 	}
-	
-	// TODO: Checksum computation and writing
 
+	uint32_t hashVal = checksum(key+value);
+
+	// Convert string to be written to char*	
+	std::string str = key + " " + value + " " + std::to_string(hashVal) + "\n";
 	// Convert string to be written to char*	
 	std::string str = key + " " + value + "\n";
 	char cstr[str.size() + 1];
